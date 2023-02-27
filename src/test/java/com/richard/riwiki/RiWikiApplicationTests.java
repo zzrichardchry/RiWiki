3d4312209d7309b9e3dd1a807622e3fdceb3123b
bobo_cat <zzrichardchry@icloud.com>
2023-02-27 00:36:31 -0500

    2.2 Initialize Spring Boot Project

---
+++ src/test/java/com/richard/riwiki/RiWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richard.riwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

39530ad8eeb77f428fc1a65098e54a426ce6b7ee
bobo_cat <zzrichardchry@icloud.com>
2023-04-01 10:41:01 -0400

    2-2 Project Init

---
+++ src/main/java/com/richard/riwiki/RiWikiApplication.java
@@ -0,0 +1,13 @@
+package com.richard.riwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiWikiApplication.class, args);
+    }
+
+}

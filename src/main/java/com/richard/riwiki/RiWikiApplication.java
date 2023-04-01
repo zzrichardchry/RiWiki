23908c5d80b454da577265bd01e182fd0345b346
bobo_cat <zzrichardchry@icloud.com>
2023-04-01 11:04:36 -0400

    2-5 Change boot information

--- src/main/java/com/richard/riwiki/RiWikiApplication.java
+++ src/main/java/com/richard/riwiki/RiWikiApplication.java
@@ -1,13 +1,21 @@
 package com.richard.riwiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class RiWikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(RiWikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(RiWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(RiWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Boot Success!!");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }

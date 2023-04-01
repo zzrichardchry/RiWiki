1b32fd4225a805eab6cad6e11dadf9eea896d3c7
bobo_cat <zzrichardchry@icloud.com>
2023-04-01 11:28:17 -0400

    2-6 Add Hello World interface

---
+++ src/main/java/com/richard/riwiki/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.richard.riwiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    /**
+     * GET, POST, PUT, DELETE
+     *
+     * /user?id=1  -- Traditional Ways
+     * /user/1     -- RESTful ways
+     * @return
+     */
+    // @PostMapping
+    // @PutMapping
+    // @DeleteMapping
+    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
+    // @RequestMapping(value = "/user/1", method = RequestMethod.DELETE)
+    // http://127.0.0.1:8880/hello
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}

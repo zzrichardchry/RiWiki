ed26510a5b90c2f08bbdaf792b1bb3f90d32880c
bobo_cat <zzrichardchry@icloud.com>
2023-04-01 19:14:53 -0400

    2-8 Add customize configure option

--- src/main/java/com/richard/riwiki/controller/TestController.java
+++ src/main/java/com/richard/riwiki/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.richard.riwiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
 
     /**
      * GET, POST, PUT, DELETE
@@ -22,7 +26,7 @@ public class TestController {
     // http://127.0.0.1:8880/hello
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!, " + testHello;
     }
 
     @PostMapping("/hello/post")
